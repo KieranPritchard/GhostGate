a5251be9b0707c8a7a0bb2067b52083e836701cb
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-05-13 20:51:35 +0100

    added clean file path

--- internal/sanitation/cleanfilepath.go
+++ internal/sanitation/cleanfilepath.go
@@ -1,11 +1,17 @@
 package sanitation
 
-import "path/filepath"
+import (
+	"path/filepath"
+	"strings"
+)
 
 // Function to clean file paths
 func CleanFilePath(filePath string) string{
+	// Trims the filepath of white space
+	trimmedPath := strings.Trim(filePath, "")
+	
 	// Cleans the filepath
-	cleanPath := filepath.Clean(filePath)
+	cleanPath := filepath.Clean(trimmedPath)
 
 	// Returns the filepath
 	return cleanPath
