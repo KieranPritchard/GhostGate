cf7d0041328d91faa44f18c4c60a7c07d72e36f4
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-05-23 15:41:44 +0100

    added audit response function

--- internal/essentail/auditrequests.go
+++ internal/essentail/auditrequests.go
@@ -1,1 +1,27 @@
 package essentail
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"time"
+)
+
+// AuditRequest connects to a HTTP servere and dumps the structure then terminates it
+func AuditRequest(response *http.Response){
+	// DumpResponse extracts the raw protocol bytes retuned by the target server.
+	// Setting the second parameter to 'true' includes the HTML/data body.
+	responseDump, err := httputil.DumpResponse(response, true)
+
+	// Checks for any errors
+	if err != nil {
+		// Prints an error
+		fmt.Printf("[!] Failed to dump server response: %v\n", err)
+		return
+	}
+
+	// Print the raw headers and server flags directly to your terminal
+	fmt.Printf("\n=== [TARGET HTTP SERVER AUDIT: %s] ===\n", time.Now().Format("15:04:05"))
+	fmt.Println(string(responseDump))
+	fmt.Println("=====================================================")
+}
