77a44411608ca670eef06892e519dd6f482faa12
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-05-26 20:41:13 +0100

    added new function

---
+++ internal/essentail/parserobots.go
@@ -0,0 +1,9 @@
+package essentail
+
+// Function to parse the robots from the input
+func ParseRobots(domain string) ([]string, []string, error){
+	// Empty lists to store the data parsed
+	var disallowed []string
+	var sitemaps []string
+	var other []string
+}
