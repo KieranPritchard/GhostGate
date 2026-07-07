b1e5aff07f37fd87fd47213035a3d55c3acae69f
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-07-07 19:33:25 +0100

    added root.go

--- cmd/root.go
+++ cmd/root.go
@@ -1,1 +1,32 @@
-package root
+package cmd
+
+import (
+	"GhostGate/internal/logger"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// Defines the root command
+var rootCmd = &cobra.Command{
+	Use: "GhostGate",
+	Long: "GhostGate is a versatile Go-based networking toolkit designed for penetration testing, red teaming, and security auditing. It simplifies the process of setting up payload staging environments, running data exfiltration handlers, establishing reverse/pivot tunnels, and conducting quick HTTP security configuration audits.",
+
+	// Creates a new logger before running each subcommand
+	PersistentPreRun: func (cmd *cobra.Command, args []string)  {
+		// Creates a new logger
+		logger.New(logger.Config{
+			Level: "INFO",
+			Format: logger.FormatText,
+		})
+	},
+}
+
+// Function to execute the root command
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+// Add global subcommands under init function
